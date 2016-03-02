ec67c17c23d7c1ce13c04a25a9b3399f17cd70f1
livysdad27 <livysdad27@gmail.com>
2016-03-02 07:45:17 -0800

    fixed x collisions

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ class Player(pygame.sprite.Sprite):
   def __init__(self, x, y):
     super(Player, self).__init__()
     self.runningAnim = animList[0]
-    self.goingLeft = True
+    self.going_left = True
     self.standing = False
     self.runningAnim.play()
 
@@ -47,15 +47,15 @@ class Player(pygame.sprite.Sprite):
     self.scan_rect = self.rect.copy()
 
     self.max_speed = 10
-    self.jump_accel = -8
+    self.jump_accel = -15
     self.dx = 0
     self.dy = 0
     self.dvx= 0
     self.dvy= 0
 
-  def check_standing(self):
+  def check_falling(self):
     test_rect = self.rect.copy()
-    test_rect.y += 1
+    test_rect.y += 2
     if len(test_rect.collidelistall(wall_rect_list)) == 0:
       self.standing = False
     else:
@@ -69,32 +69,51 @@ class Player(pygame.sprite.Sprite):
     block_hit_list = test_rect.collidelistall(wall_rect_list)
     if len(block_hit_list) == 0:
       self.dvy = GRAVITY
+      self.standing = False
     else:
       highest_top = 10000
       for block_index in block_hit_list:
         current_top = wall_rect_list[block_index].top  
         if current_top < highest_top:
           highest_top = current_top 
-      self.rect.bottom = highest_top
+      self.rect.bottom = highest_top - 1
       self.dvy = 0
       self.dy = 0
+      self.standing = True
 
   def calculate_collide(self):
     test_rect = self.rect.copy()
+    test_rect.y = self.rect.y
     test_rect.x += self.dx
     block_hit_list = test_rect.collidelistall(wall_rect_list)
     if len(block_hit_list) > 0:
       for block_index in block_hit_list:
-        pass
-        
+        if self.going_left == True:
+          furthest_right = -100
+          current_right = wall_rect_list[block_index].right
+          if current_right > furthest_right:
+            furthest_right = current_right
+          self.rect.left = furthest_right + 1
+          self.dvx = 0
+          self.dx = 0
+        elif self.going_left == False:
+          furthest_left = 10000
+          current_left = wall_rect_list[block_index].left
+          if current_left < furthest_left:
+            furthest_left = current_left
+          self.rect.right = furthest_left -1
+          self.dvx = 0
+          self.dx = 0
 
   def jump(self):
     self.dvy = self.jump_accel
+    self.standing = False
 
   def update(self):
-    self.check_standing()
+    self.check_falling()
     if self.standing == False:
       self.calculate_gravity()
+    self.calculate_collide()
 
     if self.standing == True:
       if (key_pressed(K_w)):
@@ -105,12 +124,18 @@ class Player(pygame.sprite.Sprite):
         self.dvx = -1 
       else:
         self.dvx = 0
+      if self.going_left == False:
+        self.runningAnim.flip(True, False)
+      self.going_left = True
 
     if key_pressed(K_d) == True:
       if (self.dx < self.max_speed):
         self.dvx = 1  
       else:  
         self.dvx = 0
+      if self.going_left == True:
+        self.runningAnim.flip(True, False)
+      self.going_left = False
  
     if (not key_pressed(K_d)) and (not key_pressed(K_a)):
       self.dvx = 0
@@ -139,15 +164,18 @@ wall_list = pygame.sprite.Group()
 
 wall = Wall(0, 390, 500, 100)
 wall2 = Wall(0, 290, 30, 100)
+wall3 = Wall(200, 290, 30, 100)
 wall_list.add(wall)
 wall_list.add(wall2)
+wall_list.add(wall3)
 all_sprite_list.add(wall)
 all_sprite_list.add(wall2)
+all_sprite_list.add(wall3)
 
 for wall in wall_list:
   wall_rect_list.append(wall.rect)
 
-player = Player(300, 50)
+player = Player(200, 50)
 #player.walls = wall_list
 all_sprite_list.add(player)
 
